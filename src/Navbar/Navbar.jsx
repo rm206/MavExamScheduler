4adbcc82a46c9ab700f1894026a847a00dd3ccf4
Rishabh <panicpark60@gmail.com>
2024-07-03 01:05:14 -0500

    complete welcome page

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,30 +3,6 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 import UTAImage from '../assets/UTA_Initials_white-orange-star-rgb.png';
 
-// const NavBar = () => {
-//     return (
-//         <nav className="navbar">
-//             <div className="navbar-left">
-//                 <img src={UTAImage} alt="UTA Logo" className="uta-logo" />
-//             </div>
-//             <ul className="navbar-links">
-//                 <li>
-//                     <Link to="/" className="nav-link">Home</Link>
-//                 </li>
-//                 <li>
-//                     <Link to="/calendar" className="nav-link">Calendar</Link>
-//                 </li>
-//                 <li>
-//                     <Link to="/contact" className="nav-link">Contact</Link>
-//                 </li>
-//                 <li>
-//                     <Link to="/search" className="nav-link">Search</Link>
-//                 </li>
-//             </ul>
-//         </nav>
-//     );
-// };
-
 const NavBar = () => {
     return (
         <nav className='navbar'>
@@ -51,4 +27,4 @@ const NavBar = () => {
     );
 };
 
-export default NavBar;
+export default NavBar; 
--- src/Welcome/Welcome.jsx
+++ src/Welcome/Welcome.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import './Welcome.css'
 
 export default function Welcome() {
     return (
-        <div>Welcome</div>
+        <>
+            <div class="fadeInUp-animation fadeInTitle">Welcome To Mav Exam Scheduler!</div>
+            <div className="info-container">
+                <div className="info-box fadeInUp-animation">
+                    <h2>About This Website</h2>
+                    <p>Looking at big PDF master plans like <a href="https://cdn.web.uta.edu/-/media/project/website/administration/registrar/documents/exam-schedules/spring2024-master.ashx" target="_blank">this</a> to find your exam schedule is <h2 className='emoji'>🤢</h2> </p>
+                    <p>Mav Exam Scheduler is designed to streamline your exam scheduling process. The aim is to provide an efficient and user-friendly platform to manage your academic calendar.</p>
+                    <p>Simply go to Search, choose a semester/course (or both!) and easily add your examss to your personal calendar with schedule and venue!</p>
+                </div>
+                <div className="info-box fadeInUp-animation">
+                    <h2>Disclaimer</h2>
+                    <p>While I, the author and builder, have made every effort to ensure the accuracy and reliability of the information provided, I cannot guarantee its completeness or correctness. Users are strongly advised to double-check with their instructor or refer to the master plan to ensure they have the most current and accurate information. I am not responsible for any errors or omissions, or for any outcomes related to the use of this information. </p>
+                </div>
+            </div>
+        </>
     )
 }
