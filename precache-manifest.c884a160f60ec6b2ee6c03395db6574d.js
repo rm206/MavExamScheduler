6be9c2ed4f3b8e2112635c83a841d46c8e6e83cf
Rishabh <panicpark60@gmail.com>
2024-07-04 17:46:05 -0500

    Deploy first try

---
+++ precache-manifest.c884a160f60ec6b2ee6c03395db6574d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc43931297e35ad9b0bf6bf6d4c29d90",
+    "url": "/MavExamScheduler/index.html"
+  },
+  {
+    "revision": "4540e9ecba63521c8e62",
+    "url": "/MavExamScheduler/static/css/main.eb7c3c02.chunk.css"
+  },
+  {
+    "revision": "a0aea1a7f35b9b96eeee",
+    "url": "/MavExamScheduler/static/js/2.a0140d31.chunk.js"
+  },
+  {
+    "revision": "2f58089c61d46f50cf6fed08f3e9f0eb",
+    "url": "/MavExamScheduler/static/js/2.a0140d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4540e9ecba63521c8e62",
+    "url": "/MavExamScheduler/static/js/main.8c4757d6.chunk.js"
+  },
+  {
+    "revision": "245bd89384995a28c863",
+    "url": "/MavExamScheduler/static/js/runtime-main.2761964f.js"
+  },
+  {
+    "revision": "b7a187709aa04ada8bda51cc08f0bd8f",
+    "url": "/MavExamScheduler/static/media/UTA_Initials_white-orange-star-rgb.b7a18770.png"
+  }
+]);
